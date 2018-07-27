d86614944943427b4a5f8331aa410c2c3319c094
FABIO7 <FABIO7@FABIO7>
2018-07-27 18:14:26 -0500

    rtthrthy

---
+++ SIC.Intranet.Dominio/EntidadBase.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SIC.Intranet.Dominio
+{
+    using System;
+    public abstract class EntidadBase : IDisposable
+    {
+        #region Miembros de IDisposable
+
+        public void Dispose()
+        {
+            GC.Collect();
+        }
+
+        #endregion
+    }
+}
